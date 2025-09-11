4bd72af805e3abb9348b6d38080f2dbc06f8d825
Vlad Klymenko <vladyslav.klsts@gmail.com>
2025-09-11 17:37:23 +0300

    Home page with responsive

--- wp-content/themes/betheme-child/functions/theme-shortcodes.php
+++ wp-content/themes/betheme-child/functions/theme-shortcodes.php
@@ -5483,9 +5483,9 @@ if (! function_exists('sc_blog_teaser')) {
 									if( !empty($blog_meta['date']) ){
 										$output .= '<span class="date">';
 											if( !empty($blog_meta['author']) ){
-												$output .= '<span class="label">'. esc_html($translate['at']) .' </span>';
+												$output .= '<span class="label">on</span>';
 											}
-											$output .= '<i class="icon-clock"></i> ';
+											$output .= '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none"> <path d="M2.5 10C2.5 10.9849 2.69399 11.9602 3.0709 12.8701C3.44781 13.7801 4.00026 14.6069 4.6967 15.3033C5.39314 15.9997 6.21993 16.5522 7.12987 16.9291C8.03982 17.306 9.01509 17.5 10 17.5C10.9849 17.5 11.9602 17.306 12.8701 16.9291C13.7801 16.5522 14.6069 15.9997 15.3033 15.3033C15.9997 14.6069 16.5522 13.7801 16.9291 12.8701C17.306 11.9602 17.5 10.9849 17.5 10C17.5 9.01509 17.306 8.03982 16.9291 7.12987C16.5522 6.21993 15.9997 5.39314 15.3033 4.6967C14.6069 4.00026 13.7801 3.44781 12.8701 3.0709C11.9602 2.69399 10.9849 2.5 10 2.5C9.01509 2.5 8.03982 2.69399 7.12987 3.0709C6.21993 3.44781 5.39314 4.00026 4.6967 4.6967C4.00026 5.39314 3.44781 6.21993 3.0709 7.12987C2.69399 8.03982 2.5 9.01509 2.5 10Z" stroke="#BECCD3" stroke-width="0.833333" stroke-linecap="round" stroke-linejoin="round"/> <path d="M10 10H12.9167" stroke="#BECCD3" stroke-width="0.833333" stroke-linecap="round" stroke-linejoin="round"/> <path d="M10 5.8335V10.0002" stroke="#BECCD3" stroke-width="0.833333" stroke-linecap="round" stroke-linejoin="round"/> </svg>';
 											$output .= '<span class="post-date">'. esc_html(get_the_date()) .'</span>';
 										$output .= '</span>';
 									}
